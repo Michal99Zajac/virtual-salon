0bc4e225679a6306cde4686b14051c40f6a308ca
Michal Zajac <michal.99.zajac@gmail.com>
2021-01-16 12:25:12 +0100

    refactored pages for php

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php
+  pre_r($_POST);
+  ?>
+  <form action="" method="post">
+    <input name="name" type="text">
+    <input type="submit" name="monday" value="Monday">
+    <input type="submit" name="monday" value="Thu">
+  </form>
+</body>
+</html>
 
-echo "Hello World yo!";
+<?php
+  function pre_r( $array ) {
+    echo '<pre>';
+    print_r($array);
+    echo '</pre>';
+  }
+?>
