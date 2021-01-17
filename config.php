c7ff1975d88fc77c2b3465ed506555fb71b77bbf
Michal99Zajac <50200782+Michal99Zajac@users.noreply.github.com>
2021-01-17 20:38:10 +0100

    Delete config.php

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-const USERNAME = 'acpotacgmfsugs';
-const PASSWORD = 'd0799906225fde4b932973b44007400336ba2fd26c73e93355f35084d232cc88';
-const HOST = 'ec2-3-251-0-202.eu-west-1.compute.amazonaws.com';
-const DATABASE = 'd3bqfb3pcd13l2';
